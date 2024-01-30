28df560687c527bc739fe99fe457db5352139632
yanga-m <120945621+yanga-m@users.noreply.github.com>
2024-01-30 23:11:24 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ function toggleMenu(){
     menu.classList.toggle("open");
     icon.classList.toggle("open");
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 0) {
+            document.body.classList.add("scrolling");
+        } else {
+            document.body.classList.remove("scrolling");
+        }
+    });
+});
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+}
